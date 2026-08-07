3945a2199b1486aae0e3b3fbfb8256421764a2e5
leodeveloperstore-cyber <leodeveloperstore@gmail.com>
2026-08-07 11:14:12 +0500

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -69,7 +69,19 @@ def scrape():
     bulletin_date = time.strftime("%d-%b-%Y %H:%M PKT")
     
     try:
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'Sec-Fetch-Dest': 'document',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'none',
+            'Sec-Fetch-User': '?1',
+            'Cache-Control': 'max-age=0'
+        }
         res = requests.get(FFD_URL, headers=headers, timeout=20, verify=False)
         if res.status_code == 200:
             soup = BeautifulSoup(res.text, 'html.parser')
